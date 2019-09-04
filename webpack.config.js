7e6137b97e070154bdb5e8332ee7c64bc175f76e
Dicky-L <magicpowerldz@gmail.com>
2019-09-04 15:31:03 +0800

    output management

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WebpackManifestPlugin = require('webpack-manifest-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.join(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出'
+    }),
+    new CleanWebpackPlugin(),
+    new WebpackManifestPlugin()
+  ]
 }
