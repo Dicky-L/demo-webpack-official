567c31da639956c76a9924151f631030263e037c
Dicky-L <magicpowerldz@gmail.com>
2019-09-06 18:03:30 +0800

    添加开发模式

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,23 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const WebpackManifestPlugin = require('webpack-manifest-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'inline-source-map',
   output: {
     filename: '[name].bundle.js',
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devServer:{
+    contentBase: './dist'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: '管理输出'
+      title: '开发环境'
     }),
     new CleanWebpackPlugin(),
     new WebpackManifestPlugin()
