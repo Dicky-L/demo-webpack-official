3c405cbba492a4d985585970415b823df12818e5
Dicky-L <magicpowerldz@gmail.com>
2019-09-16 10:26:52 +0800

    生产环境 production

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import _ from "lodash"
+import { cube } from './math.js'
 
-function component() {
-  let element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!')
 }
 
-document.body.appendChild(component());
+function component() {
+  let element = document.createElement('pre')
+
+  element.innerHTML = ['Hello webpack!', '5 cubed is equal to ' + cube(5)].join('\n\n')
+
+  return element
+}
+
+document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
+  devtool: 'source-map',
   output: {
     filename: 'main.js',
     path: path.join(__dirname, 'dist')
-  }
+  },
+  // optimization: {
+  //   usedExports: true
+  // }
 }
