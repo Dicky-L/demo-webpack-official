ef84bb832bd7600fcfe2517985924b0292b22240
Dicky-L <magicpowerldz@gmail.com>
2019-09-23 16:10:08 +0800

    shim 预置全局变量

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.join(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      _: 'lodash',
+    })
+  ]
 }
